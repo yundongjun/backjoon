9273a4398a3f649e9d15acd0e681eb0361be7b5a
yundongjun <68623554+yundongjun@users.noreply.github.com>
2025-04-22 10:43:32 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+
+	int n;
+	scanf("%d", &n);
+
+	for(int i = 1; i <= n; i++) {
+		for(int k = 0; k < n - i; k++)
+			printf(" ");
+		for(int k = 0; k < i + (i - 1); k++)
+			printf("*");
+
+		printf("\n");
+	}
+
+	for(int i = n - 1; i > 0; i--) {
+		for(int k = 0; k < n - i; k++)
+			printf(" ");
+		for(int k = 0; k < i + (i - 1); k++)
+			printf("*");
+
+		printf("\n");
+	}
+
+	return 0;
+}
